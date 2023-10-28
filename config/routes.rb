efa22f1fa56b461853af2f01900cf7d752885df8
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-10-28 18:04:11 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
+  resources :rooms, only: [:new, :create] do
+  resources :messages, only: [:index, :create]
+  end
 end
