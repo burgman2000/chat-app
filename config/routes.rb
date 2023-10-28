c55c2f94421517809df797e2a606a6d2e769a246
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-10-28 17:30:04 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   resources :rooms, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ↑↑上の記述は削除しましょう↑↑
-  root to: "messages#index"
 end
